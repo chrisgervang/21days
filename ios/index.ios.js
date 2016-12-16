530a9c6ea1e0585002659ba3a345e2a9ef4312d2
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-15 16:29:27 -0800

    make all 6 grids clickable. perhaps make a different function for each box to represent its habit

--- ios/index.ios.js
+++ ios/index.ios.js
@@ -23,13 +23,23 @@ export default class test extends Component {
             <TouchableHighlight onPress={this._onPressButton}>
               <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
             </TouchableHighlight>
-            <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}} />
-            <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}} />
+            <TouchableHighlight onPress={this._onPressButton}>
+              <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this._onPressButton}>
+              <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
+            </TouchableHighlight>
           </View>
           <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.05)', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-            <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}} />
-            <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}} />
-            <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}} />
+            <TouchableHighlight onPress={this._onPressButton}>
+              <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this._onPressButton}>
+              <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this._onPressButton}>
+              <View style={{height: 150, width: 150, padding: 25, backgroundColor: 'steelblue', margin: 5}}/>
+            </TouchableHighlight>
           </View>
         </View>
       </View>
