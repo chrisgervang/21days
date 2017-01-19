c0e783deb19eec20e67e8f0938999ba4dd7e7ac4
Chris Gervang <chris@gervang.com>
2017-01-18 23:57:52 -0800

    db functions

--- server/index.js
+++ server/index.js
@@ -2,17 +2,63 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.json({results: result.rows} ); }
-    });
-  });
-});
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.json({results: result.rows} ); }
+//     });
+//   });
+// });
+
+app.get('/habits', function (request, response) {
+    var id = process.evn.PARTICLE_ID;
+    if(id === request.query.id) {
+        var user = request.query.user;
+        var query = "SELECT (habit, completedDate) FROM public.habit WHERE user_email = '" + user + "'";
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query(query, function(err, result) {
+                done();
+                if(err) {
+                    console.error(err);
+                    response.send("Error " + err);
+                } else {
+                    response.send("Success " + result);
+                }
+            })
+        })
+    } else {
+        console.error("Auth did not match");
+        response.send("Error Auth did not match");
+    }
+})
+
+app.get('/track', function (request, response) {
+    var id = process.evn.PARTICLE_ID;
+    if(id === request.query.id) {
+        var habit = request.query.habit;
+        var user = request.query.user;
+        var query = "INSERT INTO public.habit (habit, user_email) VALUES ('" + habit + "', '" + user + "')";
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query(query, function(err, result) {
+                done();
+                if(err) {
+                    console.error(err);
+                    response.send("Error " + err);
+                } else {
+                    response.send("Success " + result);
+                }
+            })
+        })
+    } else {
+        console.error("Auth did not match");
+        response.send("Error Auth did not match");
+    }
+    
+})
 
 app.set('port', (process.env.PORT || 5000));
 
---
+++ server/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE public.user
+(
+    email text NOT NULL,
+    created timestamp without time zone NOT NULL DEFAULT now(),
+    profile json NOT NULL default '{}'::json,
+    PRIMARY KEY (email)
+)
+
+CREATE TABLE public.habits
+(
+    habit text,
+    CONSTRAINT habits_pkey PRIMARY KEY (habit)
+)
+
+CREATE TABLE public.habit
+(
+    completed timestamp without time zone NOT NULL DEFAULT now(),
+    completedDate date NOT NULL DEFAULT now(),
+    user_email text NOT NULL,
+    habit text NOT NULL,
+    CONSTRAINT habit_pkey PRIMARY KEY (user_email, habit, completedDate),
+    CONSTRAINT habit_user_email_fkey FOREIGN KEY (user_email)
+        REFERENCES public."user" (email) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT habit_habit_fkey FOREIGN KEY (habit)
+        REFERENCES public."habits" (habit) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+INSERT INTO public.habits (habit) VALUES ('dont murder'), ('on time'), ('workout'), ('brush twice'), ('sleep by 12am'), ('no sweets');
