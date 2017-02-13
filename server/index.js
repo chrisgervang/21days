660fd00ebd2d62b2a911cb1a254f9b745e1de43c
Chris Gervang <chris@gervang.com>
2017-02-12 22:24:19 -0800

    Es6 promise.. heroic doesn't support yet?

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var app = express();
 var pg = require('pg');
 var moment = require('moment-timezone');
 var bodyParser = require('body-parser');
+var Promise = require("es6-promise").Promise;
 
 // app.get('/db', function (request, response) {
 //   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
